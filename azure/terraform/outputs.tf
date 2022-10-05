e85f7481759c586b171f6fe9b3c9ee8954e4930a
Rune Rønneseth <35924873+kds-rune@users.noreply.github.com>
2022-10-05 16:34:37 +0200

    ups..

--- azure/terraform/outputs.tf
+++ azure/terraform/outputs.tf
@@ -6,7 +6,9 @@ output "info" {
   sensitive = true
   
   value = {
-    id     = azuread_application.MAIN.application_id
-    secret = azuread_service_principal_password.MAIN.value
+    app_id   = azuread_application.MAIN.application_id
+    secret   = azuread_service_principal_password.MAIN.value
+    rg_name  = azurerm_resource_group.MAIN.name
+    location = azurerm_resource_group.MAIN.location
   }
 }
